964b40a84dce46cdb7e1743e6850d24a23895707
andreluismoraes500 <124262969+andreluismoraes500@users.noreply.github.com>
2023-08-06 20:12:01 -0300

    Fazendo o desafio dos pontinhos

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { MouseEvent, useState } from "react";
+import "./App.css";
+
+type Dots = {
+  top: number;
+  left: number;
+};
+
+function App() {
+  const [dots, setDots] = useState<Dots[]>([]);
+  const [historyDots, setHistoryDots] = useState<Dots[]>([]);
+
+  const quandoUsuarioClica = (event: MouseEvent<HTMLElement>) => {
+    const { clientX, clientY } = event;
+    const newDots = {
+      top: clientY,
+      left: clientX,
+    };
+    setDots([...dots, newDots]);
+    setHistoryDots([...dots, newDots]);
+  };
+
+  const clicaDesfazerDots = (event: MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    if (dots.length == 0) {
+      return;
+    }
+
+    setDots(dots.slice(0, -1));
+  };
+
+  const clidaRefazerDots = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.stopPropagation();
+    event.preventDefault();
+
+    if (
+      historyDots.length - (historyDots.length - dots.length) ==
+      historyDots.length
+    ) {
+      return;
+    }
+
+    setDots([
+      ...dots,
+      historyDots[historyDots.length - (historyDots.length - dots.length)],
+    ]);
+  };
+
+  return (
+    <div className="app" onClick={quandoUsuarioClica}>
+      <button onClick={clicaDesfazerDots}>Desfazer</button>
+      <button onClick={clidaRefazerDots}>Refazer</button>
+
+      {dots.map((dot) => (
+        <div key={Math.random() * 10000} className="dot" style={dot}></div>
+      ))}
+    </div>
+  );
+}
+
+export default App;
